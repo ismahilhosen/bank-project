030b399fc43ae3b124941f85f1d436e61429d78f
ismahilhosen <ismahilhosen4000@gmail.com>
2023-08-19 13:20:25 +0600

    frist commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputEmail = document.getElementById("email")
+const inputPassword = document.getElementById("password")
+
+
+
+function loginAth(emali,password){
+    if(emali === "ismahilhosen4000@gmail.com" && password === "1234"){
+        location.href = "./index.html"
+    }else{
+        alert("password and email incorrect")
+    }
+}
+
+const submitBtn = document.getElementById("submit")
+
+document.getElementById("submit").addEventListener("click", function(){
+    loginAth(inputEmail.value,inputPassword.value);
+
+})
